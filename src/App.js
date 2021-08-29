097be682db50fdcee97d7bd942cd1dcc5885f776
Stephen Montague <stephen.d.montague@gmail.com>
2021-08-29 19:12:22 -0400

    React basics - Learning to work with components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
-}
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
+	const expenses = [
+		{
+			id: 'e1',
+			title: 'Car Insurance',
+			amount: 294.67,
+			date: new Date(2021, 8, 23),
+		},
+		{
+			id: 'e1',
+			title: 'Car Insurance',
+			amount: 294.67,
+			date: new Date(2021, 8, 23),
+		},
+		{
+			id: 'e2',
+			title: 'Mortgage',
+			amount: 1286.78,
+			date: new Date(2021, 8, 23),
+		},
+		{
+			id: 'e3',
+			title: 'Car Payment',
+			amount: 325.0,
+			date: new Date(2021, 8, 23),
+		},
+		{
+			id: 'e4',
+			title: 'Spotify',
+			amount: 12.99,
+			date: new Date(2021, 8, 23),
+		},
+	];
+
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+			<Expenses items={expenses} />
+		</div>
+	);
+};
 
 export default App;
