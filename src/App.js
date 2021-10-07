2d659ac96c9ebb117d994fd01ca11f946d1944a7
Stephen Montague <me@montaguestephen.com>
2021-10-06 22:54:26 -0400

    Learned state, how to pass info from child to parent, and stateful arrays.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
+import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+
+const DUMMY_EXPENSES = [
+	{
+		id: 'e1',
+		title: 'Car Insurance',
+		amount: 294.67,
+		date: new Date(2021, 8, 23),
+	},
+	{
+		id: 'e2',
+		title: 'Car Insurance',
+		amount: 294.67,
+		date: new Date(2021, 8, 23),
+	},
+	{
+		id: 'e3',
+		title: 'Mortgage',
+		amount: 1286.78,
+		date: new Date(2021, 8, 23),
+	},
+	{
+		id: 'e4',
+		title: 'Car Payment',
+		amount: 325.0,
+		date: new Date(2021, 8, 23),
+	},
+	{
+		id: 'e5',
+		title: 'Spotify',
+		amount: 12.99,
+		date: new Date(2021, 8, 23),
+	},
+];
 
 const App = () => {
-	const expenses = [
-		{
-			id: 'e1',
-			title: 'Car Insurance',
-			amount: 294.67,
-			date: new Date(2021, 8, 23),
-		},
-		{
-			id: 'e1',
-			title: 'Car Insurance',
-			amount: 294.67,
-			date: new Date(2021, 8, 23),
-		},
-		{
-			id: 'e2',
-			title: 'Mortgage',
-			amount: 1286.78,
-			date: new Date(2021, 8, 23),
-		},
-		{
-			id: 'e3',
-			title: 'Car Payment',
-			amount: 325.0,
-			date: new Date(2021, 8, 23),
-		},
-		{
-			id: 'e4',
-			title: 'Spotify',
-			amount: 12.99,
-			date: new Date(2021, 8, 23),
-		},
-	];
+	const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+	const addExpenseHandler = expense => {
+		setExpenses(prevExpenses => {
+			return [expense, ...prevExpenses];
+		});
+	};
 
 	return (
 		<div>
-			<h2>Let's get started!</h2>
+			<NewExpense onAddExpense={addExpenseHandler} />
 			<Expenses items={expenses} />
 		</div>
 	);
